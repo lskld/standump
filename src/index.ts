16029babdb8803fbd3ca844c3669a86c5203abee
lskld <theo.lillieskold@gmail.com>
2026-06-04 22:59:32 +0200

    setup commander with options and base commands for standump

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-console.log("standump works")
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+	.name("standump")
+	.description("Dumps your git history into a standup so you don't have to")
+	.version("1.0.0")
+	.option("--days <number>", "how many days back to look", "1")
+	.option("--author <name>", "filter by git author")
+	.option(
+		"--lang <language>",
+		"output language (english, swedish, spanish, german, french)",
+		"english",
+	)
+	.action((options) => {
+		console.log("days:", options.days);
+		console.log("author", options.author);
+		console.log("lang:", options.lang);
+	});
+
+program.parse();
+    
